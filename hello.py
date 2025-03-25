5bbadb7387e7c68ffbae70d5df181d27c3eed7ed
Shamim Ahmed <shimz.ahmed@gmail.com>
2025-03-25 06:27:21 +0500

    del hello

--- hello.py
+++
@@ -1,6 +0,0 @@
-print("Hello Shamim Ahmed.")
-response = input("How are you Shamim!")
-print("Nice to hear that your are " + response)
-print("Hello again Shamim.")
-print("Hello again Shamim1.")
-print("Hello again Branch5.")
